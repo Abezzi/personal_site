6cda362150842926da13dc1fde807d419a4a2511
wizwiz <abezzi@outlook.cl>
2024-12-19 15:21:34 -0300

    feat: theme config in root

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { LinksFunction } from "@remix-run/node"
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node"
 import stylesheet from "~/tailwind.css?url";
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
@@ -9,15 +9,38 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
+import { PreventFlashOnWrongTheme, ThemeProvider, useTheme } from "remix-themes"
+import { darkSessionResolver } from "./utils/session.server";
 
-export default function App() {
+// use a loader in order to get data
+export async function loader({ request }: LoaderFunctionArgs) {
+  const { getTheme } = await darkSessionResolver(request);
+
+  return {
+    theme: getTheme(),
+  };
+}
+export default function AppWithProvider() {
+  const { theme } = useLoaderData<typeof loader>();
   return (
-    <html lang="en">
+    <ThemeProvider specifiedTheme={theme} themeAction="/action/set-theme">
+      <App />
+    </ThemeProvider>
+  );
+}
+
+function App() {
+  const { theme } = useLoaderData<typeof loader>();
+  const [dTheme] = useTheme();
+  return (
+    <html lang="en" data-theme={dTheme ?? ''}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
+        <PreventFlashOnWrongTheme ssrTheme={Boolean(theme)} />
         <Links />
       </head>
       <body>

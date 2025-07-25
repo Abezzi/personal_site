0f84df2fc2a98eadca2e4818223ded93e05b9080
wizwiz <abezzi@outlook.cl>
2025-07-25 15:03:19 -0400

    feat(about): short about page

---
+++ app/components/About.tsx
@@ -0,0 +1,22 @@
+export default function About() {
+  return (
+    <div className="">
+      <h1 className="tracking-tighter font-bold dark:text-slate-300 text-slate-800 text-3xl md:text-4xl mx-auto m-6">
+        About
+      </h1>
+      <div className="text-justify text-xl font-sans dark:text-white text-slate-800">
+        <p>
+          My name is Alex Herrera Saravia. I'm a full-stack developer and app
+          developer enthusiast from Chile. I'm passionate about music,
+          technology and health.
+        </p>
+        <p>
+          Whether you're a business owner looking to get started on a app or web
+          development project with a freelance web developer, a developer
+          looking to collaborate on something cool, or just wanting to say hi,
+          send me a message and let's work together.
+        </p>
+      </div>
+    </div>
+  );
+}

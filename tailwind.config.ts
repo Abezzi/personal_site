46f4a5f44cde82972174529a782810f7fe2e2459
wizwiz <abezzi@outlook.cl>
2025-12-30 20:57:40 -0300

    style(tailwind): prettier format

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,14 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 export default {
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
-  darkMode: ['class', '[data-theme="dark"]'],
+  darkMode: ["class", '[data-theme="dark"]'],
   theme: {
     extend: {
       fontSize: {
-        '1em': '1em'
-      }
+        "1em": "1em",
+      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
-} satisfies Config
-
+} satisfies Config;
